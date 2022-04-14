3aa7bbfec0bd00178a9272cd5449848d66e21f3a
Rishitha0 <98759037+Rishitha0@users.noreply.github.com>
2022-04-14 14:07:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 "use strict";
-const redis = require('redis');
-const session = require('express-session');
-
-let RedisStore = require('connect-redis')(session);
-let redisClient = redis.createClient();
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
-app.use(express.json());
+
+//Session Management 
+const redis = require('redis');
+const session = require('express-session');
+ 
+let RedisStore = require('connect-redis')(session);
+let redisClient = redis.createClient();
+ 
+const sessionConfig = {
+  store: new RedisStore({ client: redisClient }),
+  secret: process.env.COOKIE_SECRET, 
+  resave: false,
+  saveUninitialized: false,
+  name: "session", // now it is just a generic name
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 8, // 8 hours
+  }
+};
+ 
+app.use(session(sessionConfig));
 
 // Allow access to static resources in the public directory
 app.use(express.static("public", {index: "index.html", extensions: ["html"]}));
 
-app.use(
-    session({
-	store: new RedisStore({ client: redisClient }),
-	secret: process.env.COOKIE_SECRET, 
-	resave: false,
-	saveUninitialized: false
-    })
-);
-
-
-
+// Controllers 
 const HostController = require("./Controllers/hostControllers");
+const UserController = require("./Controllers/userControllers");
+
+//Validators 
+const hostValidation = require("./Validators/hostValidation");
+const userValidation = require("./Validators/userValidation");
 
 app.post("/api/login", HostController.login);
 app.post("/api/host", HostController.createNewHost);
