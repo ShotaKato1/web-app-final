0fb6707dacafa0a396bf36dff5934cac91d38933
Rishitha0 <98759037+Rishitha0@users.noreply.github.com>
2022-04-20 17:08:51 -0500

    Update UserModel.js

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -4,16 +4,18 @@ const db = require("./db");
 const crypto = require("crypto"); 
 const argon2 = require('argon2');
 
-function addUser(user){
-    const sql = `
-    INSERT INTO User
-        (user)
-    VALUES
-        (@user)
-    `;
-    const adduserStmt = db.prepare(sql);
-    adduserStmt.run({user});
-} 
+async function addUser(username, password, organization, email) {
+    try{
+        const hostID = crypto.randomUUID(username);
+        const hash = await argon2.hash(password);
+        const sql = `INSERT INTO Host (userid, username,password, organization, email)
+                     VALUES (@userid, @username, @password, @organization, @email)`;
+        const stmt = db.prepare(sql);
+        stmt.run({userid, username, password, organization, email});
+    } catch(e){
+        console.error(e);
+    }  
+}
 
 function addOrganizationUser(organization){
     const sql = `
@@ -51,43 +53,33 @@ function checkTime(organization){
         amone_two = CASE @amone_two WHEN  '1' 
         THEN amone_two = @amone_two 
         ELSE amone_two = amone_two END,
-
         amtwo_three = CASE @amtwo_three WHEN '1' 
         THEN amtwo_three = @amtwo_three 
         ELSE amtwo_three = amtwo_three END,
-
         amthree_four = CASE @amthree_four WHEN '1'
          THEN amthree_four = @amthree_four 
         ELSE amthree_four = amthree_four END,
-
         amfour_five = CASE @amfour_five WHEN '1' 
         THEN amfour_five = @amfour_five 
         ELSE amfour_five = amfour_five END,
-
         amfive_six = CASE @amfive_six WHEN '1' 
         THEN amfive_six = @amfive_six 
         ELSE amfive_six = amfive_six END,
-
         amsix_seven = CASE @amsix_seven WHEN '1' 
         THEN amsix_seven = @amsix_seven 
         ELSE amsix_seven = amsix_seven END,
-
         amseven_eight = CASE @amseven_eight WHEN '1' 
         THEN amseven_eight = @amseven_eight 
         ELSE amseven_eight = amseven_eight END,
-
         ameight_nine = CASE @ameight_nine WHEN '1' 
         THEN ameight_nine = @ameight_nine 
         ELSE ameight_nine = ameight_nine END,
-
         amnine_ten = CASE @amnine_ten WHEN '1' 
         THEN amnine_ten = @amnine_ten 
         ELSE amnine_ten = amnine_ten END,
-
         amten_eleven = CASE @amten_eleven WHEN '1' 
         THEN amten_eleven = @amten_eleven 
         ELSE amten_eleven = amten_eleven END,
-
         ameleven_twelve = CASE @ameleven_twelve WHEN  '1' 
         THEN ameleven_twelve = @ameleven_twelve 
         ELSE ameleven_twelve = ameleven_twelve END,
@@ -95,55 +87,42 @@ function checkTime(organization){
         amtwelve_pmone = CASE @amtwelve_pmone WHEN '1' 
         THEN amtwelve_pmone = @amtwelve_pmone 
         ELSE amtwelve_pmone = amtwelve_pmone END,
-
         pmone_two = CASE @pmone_two WHEN '1' 
         THEN pmone_two = @pmone_two 
         ELSE pmone_two = pmone_two END,
-
         pmtwo_three = CASE @pmtwo_three WHEN '1' 
         THEN pmtwo_three = @pmtwo_three 
         ELSE pmtwo_three = pmtwo_three END,
-
         pmthree_four = CASE @pmthree_four WHEN '1' 
         THEN pmthree_four = @pmthree_four 
         ELSE pmthree_four = pmthree_four END,
-
         pmfour_five = CASE @pmfour_five WHEN  '1' 
         THEN pmfour_five = @pmfour_five 
         ELSE pmfour_five = pmfour_five END,
-
         pmfive_six = CASE @pmfive_six WHEN '1' 
         THEN pmfive_six = @pmfive_six 
         ELSE pmfive_six = pmfive_six END,
-
         pmsix_seven = CASE @pmsix_seven WHEN '1' 
         THEN pmsix_seven = @pmsix_seven 
         ELSE pmsix_seven = pmsix_seven END,
-
         pmseven_eight = CASE @pmseven_eight WHEN '1' 
         THEN pmseven_eight = @pmseven_eight 
         ELSE pmseven_eight = pmseven_eight END,
-
         pmeight_nine = CASE @pmeight_nine WHEN '1' 
         THEN pmeight_nine = @pmeight_nine 
         ELSE pmeight_nine = pmeight_nine END,
-
         pmnine_ten = CASE @pmnine_ten WHEN '1' 
         THEN pmnine_ten = @pmnine_ten 
         ELSE pmnine_ten = pmnine_ten END,
-
         pmten_eleven = CASE @pmten_eleven WHEN '1' 
         THEN pmten_eleven = @pmten_eleven 
         ELSE pmten_eleven = pmten_eleven END,
-
         pmeleven_twelve = CASE @pmeleven_twelve WHEN '1' 
         THEN pmeleven_twelve = @pmeleven_twelve 
         ELSE pmeleven_twelve = pmeleven_twelve END,
-
         pmtwelve_amone = CASE @pmtwelve_amone WHEN '1' 
         THEN pmtwelve_amone = @pmtwelve_amone 
         ELSE pmtwelve_amone = pmtwelve_amone END,
-
     WHERE
             organization = 'organization'
     `;
@@ -160,3 +139,4 @@ module.exports = {
     getUsers,
     checkTime,
 }
+
