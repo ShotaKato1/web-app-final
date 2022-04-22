68333321a64cc539db9c0917406dc4566d7f6198
Shota <shota.kato@smail.astate.edu>
2022-04-22 13:53:40 -0500

    littel add

--- public/js/updateSchedule.js
+++ public/js/updateSchedule.js
@@ -1,2 +1,55 @@
 "use strict";
 
+function getInputs(){
+    const amone_two = document.getElementById("1-2").checked;
+    const amtwo_three = document.getElementById("2-3").checked;
+    const amthree_four  = document.getElementById("3-4").checked;
+    const amfour_five = document.getElementById("4-5").checked;
+    const amfive_six = document.getElementById("5-6").checked;
+    const amsix_seven = document.getElementById("6-7").checked;
+    const amseven_eight = document.getElementById("7-8").checked;
+    const ameight_nine = document.getElementById("8-9").checked;
+    const amnine_ten = document.getElementById("9-10").checked;
+    const amten_eleven = document.getElementById("10-11").checked;
+    const ameleven_twelve = document.getElementById("11-12").checked;
+    const amtwelve_pmone = document.getElementById("12-13").checked;
+    const pmone_two = document.getElementById("13-14").checked;
+    const pmtwo_three = document.getElementById("14-15").checked;
+    const pmthree_four = document.getElementById("15-16").checked;
+    const pmfour_five = document.getElementById("16-17").checked;
+    const pmfive_six = document.getElementById("17-18").checked;
+    const pmsix_seven= document.getElementById("18-19").checked;
+    const pmseven_eight = document.getElementById("19-20").checked;
+    const pmeight_nine = document.getElementById("20-21").checked;
+    const pmnine_ten = document.getElementById("21-22").checked;
+    const pmten_eleven = document.getElementById("22-23").checked;
+    const pmeleven_twelve = document.getElementById("23-24").checked;
+    const pmtewlve_amone = document.getElementById("24-1").checked;
+
+    return{
+        one_two,
+        two_three,
+        amthree_four,
+        amfour_five,
+        amfive_six,
+        amsix_seven,
+        amseven_eight,
+        ameight_nine,
+        amnine_ten,
+        amten_eleven,
+        ameleven_twelve,
+        amtwelve_pmone,
+        pmone_two,
+        pmtwo_three,
+        pmthree_four,
+        pmfour_five,
+        pmfive_six,
+        pmsix_seven,
+        pmseven_eight,
+        pmeight_nine,
+        pmnine_ten,
+        pmten_eleven,
+        pmeleven_twelve,
+        pmtewlve_amone,
+    }
+}
