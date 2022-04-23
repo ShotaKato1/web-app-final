09095551816208cdefdf4b72feee0f03f1279afd
Rishitha0 <98759037+Rishitha0@users.noreply.github.com>
2022-04-23 14:14:59 -0500

    Update userControllers.js

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -1,32 +1,36 @@
 "use strict";
+
+const argon2 = require("argon2"); 
 const { sendStatus, send } = require("express/lib/response");
+const hostModel = require("../Models/hostModel");
+const nodemailer = require("nodemailer");
 
-async function userTimeAvailable(req, res){
-    const obj = req.body;
+function createNewHost(req,res){
+  const {username , password} = req.body; 
+  const check = await hostModel.addHost(username, password); 
 
-    if(UserModel.checktime(obj)){
-        res.send("You complete the working!");
-    }else{
-        res.redirect('/timeAvailable');
-    }
-    return await obj;
+  if(!check){
+      return res.redirect('/hostlogin');
+  }else{
+      return res.redirect('/hostRegister');
+  }
 }
 
-async function userlogin (req,res){
-    const {username , password} = req.body; ; 
+async function login (req,res){
+  const {username , password} = req.body; ; 
   if(!host){
-    return res.redirect('/userlogin');
+    return res.redirect('/hostlogin');
   }
-  const check1 = await argon2.verify(user.hash, password); 
+  const check1 = await argon2.verify(host.hash, password); 
 
   if(check1 === true){
     if(req.session.regenerate()){
-        res.redirect('/userRegister');
+        res.redirect('/hostRegister');
     }else{
-        res.redirect('/userlogin');
+        res.redirect('/hostlogin');
       }
     }else{
-    res.redirect('/userlogin');
+    res.redirect('/hostlogin');
     }
   req.session.regenerate( (err) => {
     if(err){
@@ -34,28 +38,60 @@ async function userlogin (req,res){
         return res.sendStatus(500);
       }
     req.session.isLoggedIn = true; 
-    req.session.user = {
-      "username": user.username,
-      "userID" : user.userID,
+    req.session.host = {
+      "hostname": host.hostname,
+      "hostID" : host.hostID,
     }; 
     return res.sendStatus(200); 
     }); 
 
 }
 
-async function createNewuser(req,res){
-    const {username , password} = req.body; 
-  const check = await userModel.addUser(username, password); 
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
 
-  if(!check){
-      return res.redirect('/userlogin');
-  }else{
-      return res.redirect('/userRegister');
+function sendEmail (username, subject, text, html) {
+  const message = {
+    from: process.env.EMAIL_ADDRESS,
+    to: username,
+    subject: subject,
+    text: text,
+    html: html
+  };
+  try {
+    await transporter.sendMail(message);
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+ }
+
+const text = (
+  "Thank you joing scheduling meetings!\n\n"
+);
+ 
+const html = (
+  "<h1 style=\"margin-bottom: 1rem;\">Thank you for joining our scheduling meetings!</h1>" 
+);
+ 
+function sendSchedulingWelcome (to) {
+  const emailSent = sendEmail(to, "Welcome to Our Scheduling Meetings", text, html);
+  if (emailSent) {
+    res.redirect('/timeAvailable');
+  } else {
+    res.redirect('/login');
   }
 }
 
 module.exports = {
-    userTimeAvailable,
-    userlogin,
-    createNewuser,
-}
+    login,
+    createNewHost,
+    sendEmail,
+    sendSchedulingWelcome ,
+};
