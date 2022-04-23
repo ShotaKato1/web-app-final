1b54c6466819b476d924d5b3857debd07fa77480
Rishitha0 <98759037+Rishitha0@users.noreply.github.com>
2022-04-23 14:02:59 -0500

    Update userValidation.js

--- Validators/userValidation.js
+++ Validators/userValidation.js
@@ -5,6 +5,10 @@ const Joi = require('joi');
 const {makeBodyValidator} = require("./makeValidation");
 
 const userSchema = Joi.object({
+    "password": Joi.string()
+            .token()
+            .lowercase()
+            .required(),
     "username": Joi.string()
         .token()
         .lowercase()
