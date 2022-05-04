d4203d9999c27a589ecb5babb791a89b19110c40
Rishitha0 <98759037+Rishitha0@users.noreply.github.com>
2022-05-04 08:44:16 -0500

    Update hostModel.js

--- Models/hostModel.js
+++ Models/hostModel.js
@@ -2,22 +2,18 @@
 const db = require("./db");
 var crypto = require('crypto');
 
-
 async function addHost(username, password, organization, email) {
-
     try{
         const hostID = crypto.randomUUID(username);
         const hash = await argon2.hash(password);
-        const sql = `INSERT INTO Host (hostid, hostname, hash, organization, email)
+        const sql = `INSERT INTO Host (hostID, hostname, hash, organization, email)
                      VALUES (@hostID, @hostname, @hash, @organization, @email)`;
         const stmt = db.prepare(sql);
         stmt.run({hostID, hostname, hash, organization, email});
     } catch(e){
         console.error(e);
-    }
-    
+    }  
 }
-
 
 module.exports = {
     addHost,
